b4c31c1f0d8f913918a9dfaec5350b7856d98cfc
Adrian Haugsness <ahaugsness@icloud.com>
2018-03-31 23:54:59 -0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+// import Routes from './router';
+
+export const App: React.StatelessComponent<{}> = () => (
+    <div>hi everybody</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const dev = {
+    devtool: 'inline-source-map',
+    performance: {
+     hints: false
+    },
+    mode: 'development'
+}
+
+const config = {
+    entry: './src/index.tsx',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }, 
+            {    
+                test: /\.(sass|scss)$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }] 
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve('dist')
+    },
+    devServer: {
+        contentBase: path.resolve('dist'),  
+        publicPath: "/",
+        compress: true,
+        https: true,
+        open: true,
+        port: 4000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({  
+            filename: path.resolve('dist', 'index.html'),  
+            template: path.resolve('public', 'index.html'),
+            inject: false,
+            hash: true           
+        })
+    ]
+};
+
+module.exports = env => {
+    // Use env.<YOUR VARIABLE> here:  
+    return env && env.production ? config : Object.assign(config, dev);
+};
